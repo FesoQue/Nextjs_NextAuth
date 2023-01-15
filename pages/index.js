b30b9157fb789f6873794c14e0517f05c93a0ff4
FesoQue <adefesoq@gmail.com>
2023-01-15 02:31:40 +0100

    made changes to homepage layout

--- pages/index.js
+++ pages/index.js
@@ -16,7 +16,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className={styles.main}>
-        <div className={styles.description}>
+        {/* <div className={styles.description}>
           <p>
             Get started by editing&nbsp;
             <code className={styles.code}>pages/index.js</code>
@@ -34,80 +34,70 @@ export default function Home() {
               />
             </Link>
           </div>
-        </div>
+        </div> */}
 
-        <div className={styles.center}>
-          <Image
-            className={styles.logo}
-            src="/next.svg"
-            alt="Next.js Logo"
-            width={180}
-            height={37}
-            priority
-          />
-          <div className={styles.thirteen}>
+        <div className={styles.stackWrapper}>
+          <div className={styles.center}>
             <Image
-              src="/thirteen.svg"
-              alt="13"
-              width={40}
-              height={31}
+              className={styles.logo}
+              src="/next.svg"
+              alt="Next.js Logo"
+              width={180}
+              height={37}
               priority
             />
+
+            <div className={styles.thirteen}>
+              <Image
+                src="/thirteen.svg"
+                alt="13"
+                width={40}
+                height={31}
+                priority
+              />
+            </div>
+          </div>
+          <h1>+</h1>
+          <div className={styles.center}>
+            <Image
+              className={styles.logo}
+              src="/authjs-logo.webp"
+              alt="Next.js Logo"
+              width={100}
+              height={100}
+              priority
+            />
+            <h2>Auth.js</h2>
           </div>
         </div>
 
         <div className={styles.grid}>
           <Link href="/admin" className={styles.card}>
             <h2 className={inter.className}>
-              Dashboard <span>-&gt;</span>
+              Admin Dashboard <span>-&gt;</span>
             </h2>
             <p className={inter.className}>
               Find in-depth information about Next.js features and&nbsp;API.
             </p>
           </Link>
 
-          {/* <Link
-            href="/dashboard"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
+          <Link href="#" className={styles.card}>
             <h2 className={inter.className}>
-              Learn <span>-&gt;</span>
+              Author Dashboard <span>-&gt;</span>
             </h2>
             <p className={inter.className}>
               Learn about Next.js in an interactive course with&nbsp;quizzes!
             </p>
           </Link>
 
-          <Link
-            href="#"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
+          <Link href="#" className={styles.card}>
             <h2 className={inter.className}>
-              Templates <span>-&gt;</span>
+              User Dashboard <span>-&gt;</span>
             </h2>
             <p className={inter.className}>
               Discover and deploy boilerplate example Next.js&nbsp;projects.
             </p>
           </Link>
-
-          <Link
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Deploy <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Instantly deploy your Next.js site to a shareable URL
-              with&nbsp;Vercel.
-            </p>
-          </Link> */}
         </div>
       </main>
     </>
